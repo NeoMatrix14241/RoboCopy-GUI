5b0eb6154420963a96186dd2804a2e0554e06058
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-01-08 15:20:25 +0800

    update batch file for powershell 7 support

--- robocopy-gui.bat
+++ robocopy-gui.bat
@@ -1,3 +1,25 @@
 @echo off
-start /min powershell.exe -WindowStyle Hidden -ExecutionPolicy RemoteSigned -File "robocopy.ps1"
-exit
+title "RoboCopy GUI - PowerShell Script GUI for RoboCopy CLI"
+color 0a
+cls
+
+set "scriptPath=%~dp0"
+if "%scriptPath:~-1%"=="\" set "scriptPath=%scriptPath:~0,-1%"
+
+where pwsh >nul 2>nul
+if %errorlevel% neq 0 (
+    powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('robocopy.ps1', 'robocopy-gui.bat', 'ReadMe.txt') } | Unblock-File"
+    powershell.exe -Command "Get-ChildItem -Path '%scriptPath%\setup' -Recurse -File | Unblock-File"
+    start /max powershell.exe -WindowStyle Hidden -ExecutionPolicy RemoteSigned -File "robocopy.ps1"
+) else (
+    pwsh -NoProfile -Command "exit" >nul 2>nul
+    if %errorlevel% neq 0 (
+        powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('robocopy.ps1', 'robocopy-gui.bat', 'ReadMe.txt') } | Unblock-File"
+        powershell.exe -Command "Get-ChildItem -Path '%scriptPath%\setup' -Recurse -File | Unblock-File"
+        start /max powershell.exe -WindowStyle Hidden -ExecutionPolicy RemoteSigned -File "robocopy.ps1"
+    ) else (
+        pwsh.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('robocopy.ps1', 'robocopy-gui.bat', 'ReadMe.txt') } | Unblock-File"
+        pwsh.exe -Command "Get-ChildItem -Path '%scriptPath%\setup' -Recurse -File | Unblock-File"
+        start /max pwsh.exe -WindowStyle Hidden -ExecutionPolicy RemoteSigned -File "robocopy.ps1"
+    )
+)
